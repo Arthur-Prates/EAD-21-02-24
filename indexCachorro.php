594ed750972545e0a9d4215da5f4b1e019a9cb3c
Arthur-Prates <arthurprateslima@gmail.com>
2024-02-29 16:54:14 -0300

    end, eu acho

--- indexCachorro.php
+++ indexCachorro.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -16,7 +15,7 @@
     <style>
         .efeito-vidro {
             background: rgba(255, 255, 255, 0.06);
-            box-shadow: 0 8px 32px 0 rgba(56, 64, 212, 0.37);
+            box-shadow: 0 8px 32px 0 rgba(36, 206, 95, 0.37);
             backdrop-filter: blur(13.5px);
             -webkit-backdrop-filter: blur(13.5px);
             border-radius: 10px;
@@ -44,13 +43,49 @@ include_once('nav.php');
         </div>
         <div class="container">
 
-            <div class="maincontent efeito-vidro">
 
-                <div id="grid" class="imgrid row d-flex justify-content-center aling-items-center">
+            <form action="#" method="post">
+                <div class="row">
+                    <div class="col-6">
+                        <label for="tipo">Tipo de Animal</label>
+                        <select name='tipo' id="tipo" class="form-select  text-white bg-black" aria-label="Default select example">
+                            <option class=" text-white bg-black" value="dog">CACHORRO</option>
+                            <option class=" text-white bg-black" value="cat">GATO</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <label for="limite">Quantidade de Fotos</label>
+                        <select name='limite' id="limite" class="form-select rolamentoSelect text-white bg-black " aria-label="Default select example">
+                            <?php
+                            $cont = 1;
+                            while ($cont <= 30) {
+                                ?>
+                                <option  class=" text-white bg-black" value="<?php echo $cont ?>"><?php echo $cont ?></option>
+                                <?php
+                                $cont = $cont + 1;
+                            }
+                            ?>
 
+
+                        </select>
+                    </div>
+                    <div class="mt-3 mb-5">
+                        <button type="button" class="px-5 btn verdedog" onclick="fat()">Teste</button>
+
+                    </div>
                 </div>
+        </div>
+
+    </form>
+    <div class="container">
+
+        <div class="maincontent efeito-vidro">
+
+            <div id="grid" class="imgrid row d-flex justify-content-center aling-items-center">
+
             </div>
         </div>
+    </div>
     </form>
 </div>
 
@@ -61,8 +96,9 @@ include_once('nav.php');
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
-<script src="js/apicachorro.js"></script>
+
 <script src="js/script.js"></script>
+<script src="js/apicachorro.js"></script>
 </body>
 
 </html>
